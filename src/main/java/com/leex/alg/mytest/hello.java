1c4fb98922d59a8693dc0b5fc96b16baa5f39df1
李翔 <xiang.li@xbongbong.com>
2022-05-31 19:22:08 +0800

    fix:test5555

--- src/main/java/com/leex/alg/mytest/hello.java
+++ src/main/java/com/leex/alg/mytest/hello.java
@@ -7,23 +7,8 @@ package com.leex.alg.mytest;
  */
 public class hello {
     public static void main(String[] args) {
-        ///test2
-        //test3333
 
 
-
-    //hello4444
-        sadfksajfldkajsldfjaslkdf
-                sa
-        dfasdflaskjdflkasjdflkas
-
-                fsadfklsajdfl;jsaldfjsakldf
-                sadfsakl;dfjas;ldf;as;sdfas
-                dfas;dfadasf;;;;
-
-        ;;;
-        ;sfdsadfas
-                sfsafdsa
-        sfa
+        ///55555555555555
     }
 }
